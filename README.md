9edd3f0c5be84825c46f82566d089aadf9395645
Vijay Aswadhati <vijay.aswadhati@gmail.com>
2018-03-20 06:58:18 -0700

    baseline commit
    with working lint, editorconfig, a skeleton test framework

---
+++ README.md
@@ -0,0 +1,31 @@
+# SCIM-2 Mocha Driven Tests
+
+A non-exhaustive set of test cases for SCIM-2 endpoint.
+
+In order run these tests you will need to:
+- identify a SCIM provider to test
+- register with the SCIM provider to obtain CLIENT_ID and CLIENT_SECRET
+- set the environment variables SCIM_CLIENT_ID and SCIM_CLIENT_SECRET to the above
+
+
+## Install
+
+Clone this project from github and then:
+```sh
+cd scim-test
+npm install
+npm run test
+```
+
+## Features
+
+TBD
+
+## Development
+
+TBD
+
+
+## License
+
+MIT
---
+++ src/test-scim.js
@@ -0,0 +1,71 @@
+const chai = require('chai');
+const http = require('chai-http');
+// const jwt = require('jsonwebtoken');
+// const snapshot = require('snap-shot-it');
+
+const client_id = process.env.SCIM_CLIENT_ID;
+const client_secret = process.env.SCIM_CLIENT_SECRET;
+
+// TODO: switch to modules when NodeJS supports ESM
+const expect = chai.expect;
+chai.use(http);
+
+const scimServer = 'https://conduit.productionready.io/api';
+const authServer = 'https://conduit.productionready.io/api';
+
+// NOTE: scimServer is a closure and should be defined by this point
+// const Api = () => chai.request(scimServer);
+
+describe('SCIM API', () => {
+  before((done) => {
+    console.log(`SCIM server is at ${scimServer}`);
+    done();
+  });
+
+  after((done) => {
+    console.log('Bye bye!');
+    done();
+  });
+
+  context('Non-functional requirements', () => {
+    it('none at this point', async () => {
+      // await () => {}
+    });
+  });
+
+  context('When not authenticated', () => {
+    it('TBD at this point', async () => {
+      // expect(true).to.be.false; // fail here ...
+    });
+  });
+
+  context('When authenticated', () => {
+    before('get token', async () => {
+      const authAgent = chai.request.agent(authServer);
+
+      const res = await authAgent
+        .post('/token')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send({
+          grant_type: 'client_credentials',
+          client_id, client_secret
+        });
+
+      const token = res.body.access_token;
+      const token_type = res.body.token_type;
+      const token_expires = res.body.expires_in;
+
+      expect(token).to.be.not.null;
+      expect(token_type).to.equal('bearer');
+      expect(token_expires).to.be.not.null;
+    });
+
+    after('release token', async () => {
+
+    });
+
+    it('should be able to create user', async () => {
+
+    });
+  });
+});
